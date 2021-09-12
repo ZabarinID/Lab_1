0acf6435a440c052ed7645ab14fff980aa50f2f8
ZabarinID <48248350+ZabarinID@users.noreply.github.com>
2021-09-13 00:56:41 +0300

    Update lab_1.cpp

--- lab_1.cpp
+++ lab_1.cpp
@@ -8,50 +8,32 @@ using namespace std;
 #define BUFFER_SIZE 32767
 #define BUFSIZE MAX_PATH
 
-// 2 пункт
-double freq = 0.0;
-__int64 start = 0;
-
-void Counter_begin()
-{
-    LARGE_INTEGER fr;
-    if (!QueryPerformanceFrequency(&fr))
-        cout << "Failed\n";
-
-    freq = double(fr.QuadPart);
-    printf("\n2.1     Frequency:     %u  Hz\n", fr);
-    QueryPerformanceCounter(&fr);
-    start = fr.QuadPart;
-}
-
-double Counter_after()
-{
-    LARGE_INTEGER fr;
-    QueryPerformanceCounter(&fr);
-    return double (1000000*(fr.QuadPart - start) / freq);
-}
-
 int main()
 {
-// 1.1 Получение версии ОС
+    LARGE_INTEGER t0;
+    LARGE_INTEGER t1;
+    LARGE_INTEGER fr;
+    QueryPerformanceCounter(&t0);
+
+
     OSVERSIONINFO os_info = { 0 };
     os_info.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
     GetVersionEx(&os_info);
     printf("1.1     OS version %d.%d (NT)  build %d\n", os_info.dwMajorVersion, os_info.dwMinorVersion, os_info.dwBuildNumber);
 
-// 1.2 Получение системной директории
+
     TCHAR buffer[BUFFER_SIZE];
     GetSystemDirectory(buffer, BUFFER_SIZE);
     printf("1.2     System directory:     %s \n", buffer);
 
-// 1.3 Получение названия компьютера и псевдонима текущего пользователя
+
     DWORD len = sizeof(buffer);
     GetComputerName(buffer,&len);
     printf("1.3     Computer name:        %s \n", buffer);
     GetUserName(buffer,&len);
     printf("        User name:            %s \n", buffer);
 
-// 1.4 Вывод характеристик для каждого тома
+
     char path[MAX_PATH];
     char name[MAX_PATH];
     DWORD buf_len = MAX_PATH;
@@ -66,7 +48,8 @@ int main()
         printf("\n            Path(1st): %s", path);
         if (GetDiskFreeSpaceEx(name, &free, &total, NULL)!= 0)
             {
-                cout << "\n            Total size: " << total.QuadPart << " bytes" << "\n            Free space: " << free.QuadPart << " bytes\n\n";
+                printf("\n            Total size: %lld bytes", total.QuadPart);
+                printf("\n            Free space: %lld bytes\n\n", free.QuadPart);
             }
         else{
                 printf("\n            No space information available.\n\n");
@@ -75,7 +58,7 @@ int main()
     while (FindNextVolume(hFindVol, name, BUFSIZE));
     FindVolumeClose(hFindVol);
 
-// 1.5 Получение списка программ из реестра Windows
+
     TCHAR names[BUFFER_SIZE];
     TCHAR data[BUFFER_SIZE];
     DWORD size_name;
@@ -98,9 +81,13 @@ int main()
     } while (1);
     RegCloseKey(hkey);
 
-// 2 Измерение производительности ЦП
-    Counter_begin();
-    cout << "2.2     Measured in:   " << Counter_after()  << "  us \n";
+
+    QueryPerformanceCounter(&t1);
+    QueryPerformanceFrequency(&fr);
+    printf("\n2.1     Frequency:     %u  Hz\n", fr);
+
+    double time = 1000000 * (t1.QuadPart - t0.QuadPart) / fr.QuadPart;
+    printf("2.2     Measured in:   %f us\n", time);
 
     return 0;
 }
